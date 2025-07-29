e2cd9d5f2740419a2df06490ee08d6e807d61612
Navneet Kumar Saini <23BAI70231@cuchd.in>
2025-07-29 23:17:55 +0530

    Updated Experiment-1 files

--- Experiment-1/script.js
+++ Experiment-1/script.js
@@ -1,28 +1,10 @@
 function showMessage() {
-  const name = document.getElementById("nameInput").value.trim();
-  const email = document.getElementById("emailInput").value.trim();
-  const age = document.getElementById("ageInput").value.trim();
-<<<<<<< HEAD
+  const nameInput = document.getElementById("nameInput");
+  const name = nameInput.value.trim();
 
-  if (name === "" || email === "" || age === "") {
-    alert("⚠️ Please fill all fields!");
+  if (name === "") {
+    alert("Please enter your name before registering.");
   } else {
-    alert(
-      `${name}, you have successfully registered!\nEmail: ${email}\nAge: ${age}`
-    );
-=======
-  const outputDiv = document.getElementById("outputMessage");
-
-  if (name === "" || email === "" || age === "") {
-    alert("Please fill all the fields before registering.");
-    outputDiv.innerHTML = ""; 
-  } else {
-    const message = `
-       ${name}, you have successfully registered!<br>
-       Email: ${email}<br>
-       Age: ${age}
-    `;
-    outputDiv.innerHTML = message;
->>>>>>> 761cb189ee24b1c275af15000d21763a29a9b77b
+    alert("✅ " + name + ", you have successfully registered!");
   }
 }
